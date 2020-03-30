6f3ddd566cbf0b108fa2cfd47aa07b0214dd2429
andrioc <andrio.rodrigo.silva@hotmail.com>
2020-03-30 20:15:43 -0300

    adding files

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Title} from './styles'
+
+export default function Main(){
+  return (
+    <Container>
+      <h1>
+        Repositórios
+      </h1>
+    </Container>
+  )
+}
