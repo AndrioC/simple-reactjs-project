e361d2a2d8c5566d429881b7c53461a437c0b66b
andrioc <andrio.rodrigo.silva@hotmail.com>
2020-03-30 21:22:06 -0300

    new

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {FaGithubAlt, FaPlus, FaSpinner} from 'react-icons/fa'
 import api from '../../services/api'
 
-import {Container, Form, SubmitButton} from './styles'
+import {Container, Form, SubmitButton, List} from './styles'
 
 export default class Main extends Component{
 
@@ -37,7 +37,7 @@ export default class Main extends Component{
   }
 
   render(){
-    const {newRepo, loading} = this.state
+    const {newRepo, repositories, loading} = this.state
   return (
     <Container>
       <h1>
@@ -58,6 +58,15 @@ export default class Main extends Component{
           <FaPlus color="#FFF" size={14}/> }
         </SubmitButton>
       </Form>
+
+      <List>
+        {repositories.map(repository => (
+          <li key={repository.name}>
+            <span>{repository.name}</span>
+            <a href="">Details</a>
+          </li>
+        ))}
+      </List>
     </Container>
   )
 }
